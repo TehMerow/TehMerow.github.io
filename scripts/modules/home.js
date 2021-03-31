ed3c4fa7b6850ac902fb0f4459f359daf47e31f1
TehMerow <64508177+TehMerow@users.noreply.github.com>
2021-03-30 21:40:50 -0700

    added dividers and changed color scheme

--- scripts/modules/home.js
+++ scripts/modules/home.js
@@ -5,14 +5,19 @@ import Social_Card from "./components/social_card.js"
 
 
 const home_template_string = `
-<div id="home">
+<article id="home">
     <div id="banner-image" style="background-image: url('assets/images/itch_banner.png')"></div>
-    <div id="block" class="load-anim">
+    <section id="block" class=" light">
         <h1>Hello There</h1>
         <p>I'm TehMerow, and I make all sorts of things online!</p>
         <p>You can also Call me Merow</p>
-    </div>
-    <div id="block" class="home-social load-anim">
+        </section>
+        <div id="divider" class="divider-light" style="transform: scaleY(-1)">
+            <svg  viewBox="0 0 328.46 50.649">
+            <path d="m2.1094e-7 6.7017e-7s35.185 14.202 60.476 15.875c25.291 1.6726 57.976-12.514 88.859-11.132 30.883 1.3817 62.936 21.487 93.738 20.377 30.802-1.1103 85.388-25.119 85.388-25.119v50.649h-328.46z" stroke-linejoin="round"/>
+            </svg>
+        </div>
+        <section id="block" class="home-socialdark">
         <h3>You can find me all over the place like</h3>
         <div id="social-links">
             
@@ -25,7 +30,7 @@ const home_template_string = `
                 ></social-card>
         
             
-        </div>
+        </section>
 
 
 
